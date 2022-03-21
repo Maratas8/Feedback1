c24cc05f5447b0efc1c6cf60b5cb6cb4ddb08c82
Marius Atgalainis <maratas@gmail.com>
2022-03-21 15:05:55 +0200

    useContext implemented

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
 import Card from "./shared/Card";
 import RatingSelect from "./RatingSelect";
 import Button from "./shared/Button";
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackForm({ handleAdd }) {
+function FeedbackForm() {
   const [text, setText] = useState("");
   const [rating, setRaiting] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
+
+  const { addFeedback, feedbackEdit, updateFeedback } =
+    useContext(FeedbackContext);
+
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false);
+      setText(feedbackEdit.item.text);
+      setRaiting(feedbackEdit.item.rating);
+    }
+  }, [feedbackEdit]);
 
   const handleTextChange = (e) => {
     if (text === "") {
@@ -29,20 +41,24 @@ function FeedbackForm({ handleAdd }) {
     if (text.trim().length > 10) {
       const newFeedback = {
         text,
-        rating
+        rating,
+      };
+
+      if ((feedbackEdit.edit = true)) {
+        updateFeedback(feedbackEdit.item.id, newFeedback);
+      } else {
+        addFeedback(newFeedback);
       }
 
-      handleAdd(newFeedback);
-
-      setText('');
+      setText("");
     }
-  }
+  };
 
   return (
     <Card>
       <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
-        <RatingSelect select={(rating) => setRaiting(rating)}/>
+        <RatingSelect select={(rating) => setRaiting(rating)} />
         <div className="input-group">
           <input
             onChange={handleTextChange}
--- feedback-app/src/components/FeedbackStats.jsx
+++ feedback-app/src/components/FeedbackStats.jsx
@@ -1,22 +1,22 @@
-import PropTypes from 'prop-types';
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackStats({ feedback }) {
-    let average = feedback.reduce((acc, cur) => {
-        return acc + cur.rating;
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext);
+
+  let average =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating;
     }, 0) / feedback.length;
 
-    average = average.toFixed(1).replace(/[.,]0$/, '');
+  average = average.toFixed(1).replace(/[.,]0$/, "");
 
   return (
     <div className="feedback-stats">
-        <h4>{feedback.length} Reviews</h4>
-        <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
     </div>
-  )
+  );
 }
 
-FeedbackStats.protoTypes = {
-    feedback: PropTypes.array.isRequired,
-}
-
-export default FeedbackStats
+export default FeedbackStats;
